b78d3af0d61f97704242e11344fa1a7feb05bf24
Dexter <dhunterlaroche@gmail.com>
2020-03-30 22:13:29 -0400

    Added music!
    Added Title Music and placeholder Dungeon Music

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    void Awake()
+    {        
+        GameObject[] musdest = GameObject.FindGameObjectsWithTag("music_dest");
+        if (musdest.Length > 1)
+            Destroy (this.gameObject);
+        
+        GameObject[] mus = GameObject.FindGameObjectsWithTag("music");
+        if (mus.Length == 1)
+            Destroy (mus[0]);
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Awake()
+    {   
+        GameObject[] mus = GameObject.FindGameObjectsWithTag("music");
+        if (mus.Length > 1)
+            Destroy (this.gameObject);
+        
+        GameObject[] musdest = GameObject.FindGameObjectsWithTag("music_dest");
+        if (musdest.Length == 1)
+            Destroy (musdest[0]);
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
